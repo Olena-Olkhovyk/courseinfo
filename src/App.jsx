d1b3e9ff0bcc2d7876b80318f2e47090cca143a7
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-05-03 12:23:21 +0200

    Render components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-function App() {
-  return <></>;
-}
+import Header from "./part1/courseinfo/Header";
+import Content from "./part1/courseinfo/Content";
+import Total from "./part1/courseinfo/Total";
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header name={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exer1={exercises1}
+        exer2={exercises2}
+        exer3={exercises3}
+      />
+
+      <Total number={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
 
 export default App;
